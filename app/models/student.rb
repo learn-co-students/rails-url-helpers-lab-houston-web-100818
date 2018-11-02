51eec1eba34a8b2988c5505fa11601de8ec65b3b
jordan-laird <jordanmack.laird@gmail.com>
2018-11-02 16:33:19 -0500

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,15 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def toggle
+      if self.active == false 
+         self.active = true 
+         self.save
+       elsif self.active == true 
+         self.active = false 
+         self.save
+       end 
+
+  end
 end
