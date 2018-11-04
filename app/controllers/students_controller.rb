46130bbeaf0d9e46b6ff65973125f888e72df550
ch0mper <hannahchang17@gmail.com>
2018-11-04 16:14:35 -0600

    skipped activate

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,24 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    student_path(@student)
+    if @student.active
+      @status = "active"
+    else
+      @status = "inactive"
+    end
+  end
+
+  def activate_route
+    #activate_student_path(@student)
+    @student = Student.find(params[:id])
+    @student.activate
+    redirect_to student_path(@student)
   end
 
   private
